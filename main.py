bbb43efd51586b26f6ebdafb505322abd1a88e1a
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2025-04-09 20:28:04 -0700

    created basic window

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import pygame
+from settings import *
 
 pygame.init()
+
+
+class PyTris:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_HEIGHT))
+        pygame.display.set_caption("PyTris")
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = PyTris()
+
+    game.run()
